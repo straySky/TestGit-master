04787db9cb7adbf27802946dc4505a68f055b069
wx.s <1059455914@qq.com>
2017-05-08 14:19:15 +0800

    ffff

---
+++ app/src/main/java/com/example/administrator/testgit/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int aa = 0;
+        int bb = 1;
+        int cc = 0;
+        int ff=0;
+        System.out.println(cc + "");
+//        int dd = 0;99
+    }
+}
